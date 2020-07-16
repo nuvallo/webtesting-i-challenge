dba7f3d26695dcb5871e6564d95a1e7724fb497c
nuvallo <nuvallo.inc@gmail.com>
2020-07-16 18:58:33 -0400

    repair complete

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -71,4 +71,19 @@ describe("enhancers module", () => {
       expect(actualOutput).toMatchObject(expectedOutput);
     });
   });
+  describe("repair function", () => {
+    it("repairs the item", () => {
+      const expectedOutput = {
+        name: "sword",
+        durability: 100,
+        enhancement: 10,
+      };
+      const actualOutput = enhancer.repair({
+        name: "sword",
+        durability: 9,
+        enhancement: 10,
+      });
+      expect(actualOutput).toMatchObject(expectedOutput);
+    });
+  });
 });
